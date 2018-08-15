1ccf162557a856a53da4a69db9b26aaaac38086e
admin <admin@UNKNOWN.(none)>
2018-08-15 22:25:22 +0100

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 Rails.application.routes.draw do
-  
-  
-  
-namespace :api do 
-  namespace :v1 do 
-    resources :users do 
-      resources :trips
-    end
-    scope do
-      post   "/login"       => "sessions#create"
-      delete "/logout"      => "sessions#destroy"
+  namespace :api do 
+    namespace :v1 do 
+      resources :users do 
+        resources :trips
+      end
+      scope do
+        post   "/login"   => "sessions#create"
+        delete "/logout"  => "sessions#destroy"
+      end
     end
   end
-end
-
-  
 end
