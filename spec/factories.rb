cdb0f01d942d660a81d62ec351eae14a73e9ed66
admin <admin@admins-MacBook-Air.local>
2018-08-29 11:43:02 +0100

    Update tests to include member and admin tests.

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,18 +2,26 @@
 FactoryBot.define do
   
     factory :user do
-      name { "John" }
+      name { Faker::Name.first_name }
       email  { Faker::Internet.email }
       password { "123456" }
       password_confirmation { "123456" }
-      admin { true }
+      admin { false }
     end
+
+    factory :admin, class: User do
+        name { Faker::Name.first_name }
+        email  { Faker::Internet.email }
+        password { "123456" }
+        password_confirmation { "123456" }
+        admin { true }
+      end
 
     # Create Trip with user ID of 1 
     factory :trip do 
         start_date { Time.zone.now.to_datetime }
         end_date { Time.zone.now.to_datetime }
-        destination {"Rome"}
+        destination { Faker::Address.city }
         departure_airport_code { "GHJSY789" }
         arrival_airport_code {"HJKHJK78"}
         departure_time { Time.zone.now.to_datetime }
