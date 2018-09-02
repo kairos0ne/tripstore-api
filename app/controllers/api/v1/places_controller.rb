2851338a527b2b6b94a02b805577227e7791c3dd
admin <admin@admins-MacBook-Air.local>
2018-09-02 19:56:04 +0100

    Get all results from Google API

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -12,12 +12,22 @@ module Api
           if current_user.admin == true
             @client = GooglePlaces::Client.new(ENV["GOOGLE_API_KEY"])
             destination = Destination.find(params[:destination_id])
-            @places = @client.spots_by_query('Nearby ' + destination.title, :types => ['restaurant', 'bar', 'food', 'point_of_interest'])
+            places = @client.spots_by_query('Nearby ' + destination.title, :types => ['restaurant', 'bar', 'food', 'point_of_interest'], multipage: true)
+            # while there is a next page token specified, keep processing
+            while places.last.nextpagetoken.present?
+              places.push(@client.spots_by_pagetoken(places.last.nextpagetoken))
+            end
+            @places = places
             render json: @places  
           elsif current_user.id == user.id 
             @client = GooglePlaces::Client.new(ENV["GOOGLE_API_KEY"])
             destination = Destination.find(params[:destination_id])
-            @places = @client.spots_by_query('Nearby ' + destination.title, :types => ['restaurant', 'bar', 'food', 'point_of_interest'])
+            places = @client.spots_by_query('Nearby ' + destination.title, :types => ['restaurant', 'bar', 'food', 'point_of_interest'], multipage: true)
+            # while there is a next page token specified, keep processing
+            while places.last.nextpagetoken.present?
+              places.push(@client.spots_by_pagetoken(places.last.nextpagetoken))
+            end
+            @places = places
             render json: @places  
           else
             render :json => {:error => "You don't have permissions to visit this endpoint"}.to_json, :status => :forbidden
