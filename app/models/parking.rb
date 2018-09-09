dd1162103c5215a123e6478b4b9038fab4a78c7f
admin <admin@admins-MacBook-Air.local>
2018-09-09 15:39:14 +0100

    Render time only (needs work)

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -1,3 +1,8 @@
 class Parking < ApplicationRecord
   belongs_to :user
+
+  def time_formats
+    attributes['ArrivalTime'].strftime("%H:%M:%S")
+    attributes['DepartureTime'].strftime("%H:%M:%S")
+  end
 end
