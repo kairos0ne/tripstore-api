fb77a4414418f6fcc08a981986bcd1867159f13e
admin <admin@admins-MacBook-Air.local>
2018-09-11 11:18:39 +0100

    update from email

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,5 +1,5 @@
 class NotificationsMailer < ApplicationMailer
-    default :from => 'sdpersonal.sd@gmail.com'
+    default :from => 'dont-reply@tripstore.com'
      # send a signup email to the user, pass in the user object that   contains the user's email address
     def password_reset(user)
         @user = user
